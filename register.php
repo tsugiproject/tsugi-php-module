7962bb1aa7118b50ac54261edf4f43dd20700c26
Charles Severance <csev@umich.edu>
2016-04-10 16:51:02 -0400

    Initial commit

---
+++ register.php
@@ -0,0 +1,10 @@
+<?php
+
+$REGISTER_LTI2 = array(
+"name" => "Sample Tsugi Module",
+"FontAwesome" => "fa-university",
+"short_name" => "Sample Module",
+"description" => "This is mostly code to copy and adapt.",
+"messages" => array("launch", "launch_grade")
+);
+
